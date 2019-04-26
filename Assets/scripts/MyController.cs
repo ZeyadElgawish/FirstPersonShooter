69a9eb73b94f8be81502dffb1b9ac447b09eb86f
zeyadelgawish <Zelgawish@gmail.com>
2019-04-25 20:09:58 -0400

    New Update

---
+++ Assets/scripts/MyController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyController : MonoBehaviour
+{
+    public float speed = 2.0f;
+   // private Rigidbody rb;
+
+    //Vector3 input;
+    //public GameObject go;
+    private CharacterController mController;
+    public float gravity = 9.8f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        mController = this.gameObject.GetComponent<CharacterController>();
+        
+
+     //   rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movX = Input.GetAxis("Horizontal") * Vector3.right * speed;
+        Vector3 movZ = Input.GetAxis("Vertical") * Vector3.forward * speed;
+        Vector3 mov=transform.TransformDirection(movX+movZ);
+        mov.y -= gravity * Time.deltaTime;
+
+        mController.Move(mov);
+        
+
+
+    }
+}
---
+++ Assets/scripts/TimerUI.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerUI : MonoBehaviour
+{
+    float TimeLeft = 10f;
+    GameObject Text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimeLeft -= Time.deltaTime;
+
+        if (TimeLeft < 0)
+        {
+            GameOver();
+
+        }
+    }
+
+    private void GameOver()
+    {
+        GUI.Label(new Rect(10, 10, 100, 20), " Game Over");
+    }
+}
